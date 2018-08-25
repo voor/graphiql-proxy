d1a79f0c3cdc656bebf9420f9f0381aca2c9eb9f
Robert Van Voorhees <rcvanvo@gmail.com>
2018-08-25 14:45:07 -0400

    Fixing dumb mistake in server container.

--- scripts/start-e2e.sh
+++ scripts/start-e2e.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 echo "Executing UI Tests..."
-echo "Checking for Webdriver availability..."
-wget -O/dev/null -q --no-proxy ${SELENIUM_ADDRESS}/status --retry-connrefused --tries=100 --waitretry=1
-echo "Checking for target UI availability..."
-wget -O/dev/null -q --no-proxy ${BASE_URL} --retry-connrefused --tries=100 --waitretry=1
+echo -n "Checking for Webdriver availability ..."
+wget -O/dev/null -q --no-proxy ${SELENIUM_ADDRESS}/status --retry-connrefused --tries=100 --waitretry=1 >/dev/null 2>&1
+echo " done"
+echo -n "Checking for target UI availability ..."
+wget -O/dev/null -q --no-proxy ${BASE_URL} --retry-connrefused --tries=100 --waitretry=1 >/dev/null 2>&1
+echo " done"
 
 set -e
 
-echo "Making sure dependencies are present..."
-npm install --quiet --no-color
+echo -n "Making sure dependencies are present ..."
+npm install --quiet --no-color >/dev/null 2>&1
+echo " done"
 echo "Beginning tests..."
 npm run test:ui
